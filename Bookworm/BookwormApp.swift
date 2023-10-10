208e901e18b400e2668e52dcb7987a8c41b547e5
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-10 19:48:49 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Marcelo Amaral Alves on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
